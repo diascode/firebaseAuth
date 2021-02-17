02fb795c30d2ed7de1a449e328997544c05d0710
diascode <thiagotupa@hotmail.com>
2021-02-17 22:12:45 +0000

    add firebase to the project

--- authFire/src/app/app.module.ts
+++ authFire/src/app/app.module.ts
@@ -7,14 +7,21 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-
-
-
+import { AngularFireModule } from 'angularfire2'
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { environment } from '../environments/environment';
+import {AngularFireAuthModule} from 'angularfire2/auth';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule,
+     IonicModule.forRoot(),
+     AppRoutingModule,
+     AngularFireAuthModule,
+     AngularFirestoreModule,
+     AngularFireModule.initializeApp(environment.firebaseConfig)
+    ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
   
--- authFire/src/environments/environment.ts
+++ authFire/src/environments/environment.ts
@@ -3,7 +3,18 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig :{
+    apiKey: "AIzaSyBCkeJoz_bdlszKq3mIxM-9ggm0v64oCHI",
+    authDomain: "fidelidade-4bee0.firebaseapp.com",
+    databaseURL: "https://fidelidade-4bee0.firebaseio.com",
+    projectId: "fidelidade-4bee0",
+    storageBucket: "fidelidade-4bee0.appspot.com",
+    messagingSenderId: "793100069798",
+    appId: "1:793100069798:web:130b8ac4646d1e045cbd67",
+    measurementId: "G-Y71V3R8G3W"
+  }
+
 };
 
 /*
